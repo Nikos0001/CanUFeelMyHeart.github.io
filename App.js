46bd7434197bc06207726b4b94e3c48fac39c3dc
Mikhail Merkushov <65093651+CanUFeelMyHeart@users.noreply.github.com>
2022-04-28 21:46:52 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavigationRouter as Router } from "./components/NavigationComponent/NavigationComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <Router></Router>
+    </div>
+  );
+}
+
+export default App;
